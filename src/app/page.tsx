f0265a53be7d92dffaf5d5164398a2dcf5a208b5
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-01-12 12:22:39 +0100

    La til lenker til undersidene på forsiden.
    Co-authored-by: Runar Skagestad <runska@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
-import TestIncrementer from "./components/TestIncrementer";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <TestIncrementer />
+      <h1>Arbeidsgiverkartlegging - Testsider:</h1>
+      <ol>
+        <li>
+          <a href="/karglegging-id/deltaker/ferdig">Deltaker/Ferdig</a>
+        </li>
+        <li>
+          <a href="/karglegging-id/deltaker/sporsmal">Deltaker/Sporsmal</a>
+        </li>
+        <li>
+          <a href="/karglegging-id/deltaker/landing">Deltaker/landing</a>
+        </li>
+        <li>
+          <a href="/karglegging-id/vert/ferdig">Vert/Ferdig</a>
+        </li>
+        <li>
+          <a href="/karglegging-id/vert/oversikt">Vert/Oversikt</a>
+        </li>
+        <li>
+          <a href="/karglegging-id/vert/sporsmal">Vert/Sporsmal</a>
+        </li>
+        <li>
+          <a href="/karglegging-id/vert/start">Vert/Start</a>
+        </li>
+      </ol>
     </main>
   );
 }
