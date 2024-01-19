9297b23d512b4a11146584320ed757bfd114555c
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-01-19 12:17:19 +0100

    Oppdaterte dummydata med mer riktig spørsmål.

--- src/utils/dummydata.ts
+++ src/utils/dummydata.ts
@@ -4,29 +4,54 @@ import { bliMedDTO } from "@/app/_types/bliMedDTO";
 
 export const dummySpørreundersøkelse: spørreundersøkelseDTO = [
   {
-    id: "ab0fa573-2fde-4bdb-8913-aa3ef7d774df",
-    spørsmål: "Et spørsmål med to alternativer",
+    id: "ef4d406d-abc2-4ed6-8de7-72a7feb40326",
+    spørsmål:
+      "Hvilke av disse faktorene tror du har størst innflytelse på sykefraværet der du jobber?",
     svaralternativer: [
-      { id: "cab2214b-975b-412b-9524-7f9e908c6f31", tekst: "A" },
-      { id: "fb8bc8e5-ad82-4de8-860e-21a218cdac13", tekst: "B" },
+      {
+        id: "fbc953f1-e7fe-472d-b069-7698c4cae0a5",
+        tekst: "Arbeidsbelastning",
+      },
+      { id: "c7c0e3b2-7ef3-4256-91b8-628a333942dc", tekst: "Arbeidstid" },
+      { id: "4d4680e5-1db7-42f4-b9ce-3a3a159595c9", tekst: "Arbeidsforhold" },
+      { id: "cb8c209b-9a73-490a-8fc7-def1c9f8d7db", tekst: "Ledelse" },
+      { id: "2c11ed86-d7c9-415b-9ca0-42499a0cda61", tekst: "Noe annet" },
     ],
   },
   {
-    id: "a9f2d339-bbcd-4651-9794-fa5740c4edb8",
-    spørsmål: "Ananas på pizza?",
+    id: "153e4d26-3c89-4097-9ddb-5cfef99bc4e9",
+    spørsmål:
+      "Velg det tiltaket som du mener best kan bidra til å forebygge sykefraværet",
     svaralternativer: [
-      { id: "be9c44d6-f4bb-44fb-b4e4-d363e13f39f1", tekst: "JA" },
-      { id: "517f047a-91c9-44ed-950a-c1f628118a69", tekst: "NEI" },
+      {
+        id: "867f3ba8-8cc2-43e7-b6ef-43abb17d5609",
+        tekst: "Bedre oppfølging av ansatte",
+      },
+      {
+        id: "61b90749-4824-40c6-a99c-1bc3becfd48a",
+        tekst: "Tilrettelegging av arbeidsoppgaver",
+      },
+      {
+        id: "a2786210-9004-4540-b2c7-64a28b59c448",
+        tekst: "Kompetanseutvikling",
+      },
+      {
+        id: "db5f5b9d-928b-4485-9412-a256c233a83b",
+        tekst: "Helsefremmende aktiviteter",
+      },
+      { id: "70121318-4ff0-4bff-af75-39018b0f5c3e", tekst: "Noe annet" },
     ],
   },
   {
-    id: "903371a4-14cf-4a03-8e00-a50392bf351e",
-    spørsmål: "A, B, C eller D?",
+    id: "37294476-815b-4ed5-90ea-8189a6bc48e8",
+    spørsmål:
+      "Vi har kunnskap og ferdigheter til å gjennomføre forbedringstiltak i virksomheten (planlegge tiltak , gjennomføre og evaluere måloppnåelse)",
     svaralternativer: [
-      { id: "710f032f-7d50-4b0c-af98-aed20706f9f0", tekst: "A" },
-      { id: "1d50bd61-ca2a-4173-84ee-24ee742e0cf0", tekst: "B" },
-      { id: "4be0b0a5-28e8-4403-8392-414ec290e473", tekst: "C" },
-      { id: "56aa0482-da92-416d-88ac-1e96d96d20a3", tekst: "D" },
+      { id: "a5f6656c-9659-4f94-bfb3-d4c3ae006800", tekst: "Helt uening" },
+      { id: "0e724ae3-62c2-4164-9709-0ae651e3e902", tekst: "Litt uening" },
+      { id: "b689cbc5-13f9-4c36-9a65-ef436e4496db", tekst: "Litt ening" },
+      { id: "c350140f-8c70-4cde-a29d-1b78979f88b1", tekst: "Veldig ening" },
+      { id: "411e0247-0e52-4757-8ab7-a71f9f00b81e", tekst: "Vet ikke" },
     ],
   },
 ];
