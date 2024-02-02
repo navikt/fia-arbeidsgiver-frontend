95b1c2a3eb13bb7fad651257e3ac4cdd9ce290bd
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-02-02 09:41:36 +0100

    Fikset en til linterfeil.

--- src/app/_components/HeaderVert.tsx
+++ src/app/_components/HeaderVert.tsx
@@ -3,13 +3,13 @@ import styles from "./komponenter.module.css";
 import React from "react";
 import { Deltakere } from "@/app/_components/Deltakere";
 
-export default function HeaderVert({ deltakere }: { deltakere: number }) {
+export default function HeaderVert() {
   return (
     <Page.Block as={"header"} className={styles.header}>
       <Heading spacing level={"1"} size={"large"}>
         IA kartleggingsmøte
       </Heading>
-      <Deltakere deltakere={deltakere} />
+      <Deltakere />
     </Page.Block>
   );
 }
