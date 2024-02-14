cc718af5add3aaeadd6a2226b888a01b61086e7b
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-02-14 09:56:43 +0100

    Å kjøre "./run.sh -k" dreper nå gamle prosesser og kjører siden etterpå.

--- run.sh
+++ run.sh
@@ -7,32 +7,31 @@ while getopts 'hmdki' opt; do
       echo "  -h denne hjelpeteksten"
       echo "  -m kjører mocks-server"
       echo "  -d kjører opp siden i dev-modus"
-      echo "  -k drep serveren som kjører i bakgrunnen"
+      echo "  -k drep serveren som kjører i bakgrunnen og så kjør opp siden med mocks"
       echo "  -i installerer avhengigheter"
       exit 0
       ;;
 
     m)
-	  bun run mocks
+    bun run mocks
       ;;
 
     d)
       bun run dev
       ;;
 
-	k)
-	  killall next-server
-	  exit 0
-	  ;;
-	i)
-	  bun install
-	  exit 0
-	  ;;
-
-    ?)
-      echo -e "Ugyldig argument. Bruk: $(basename $0) [-h] [-m] [-d] [-k] [-i]"
-      exit 1
+    k)
+      killall next-server
       ;;
+    i)
+      bun install
+      exit 0
+      ;;
+
+      ?)
+        echo -e "Ugyldig argument. Bruk: $(basename $0) [-h] [-m] [-d] [-k] [-i]"
+        exit 1
+        ;;
   esac
 done
 shift "$(($OPTIND -1))"
