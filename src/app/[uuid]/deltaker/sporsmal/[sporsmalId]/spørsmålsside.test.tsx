ed6149d1e82b16cbf0b240edb059c0d9d35a8eae
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-02-26 12:57:12 +0100

    La til mer utdypende tester av spørsmålsside.

--- src/app/[uuid]/deltaker/sporsmal/[sporsmalId]/spørsmålsside.test.tsx
+++ src/app/[uuid]/deltaker/sporsmal/[sporsmalId]/spørsmålsside.test.tsx
@@ -4,6 +4,9 @@ import Spørsmålsside from "./page";
 // @ts-ignore
 import { dummySpørreundersøkelse } from "@/utils/dummydata";
 import { axe, toHaveNoViolations } from "jest-axe";
+import { svaralternativDTO } from "@/app/_types/sporreundersokelseDTO";
+import { postEnkeltSvar } from "@/app/_api_hooks/svar";
+import { useRouter } from "next/navigation";
 
 // @ts-ignore
 global.fetch = jest.fn(() =>
@@ -12,12 +15,10 @@ global.fetch = jest.fn(() =>
   }),
 );
 jest.mock("next/navigation", () => ({
-  useRouter() {
-    return {
-      prefetch: () => null,
-      push: () => null,
-    };
-  },
+  useRouter: jest.fn(() => ({
+    prefetch: () => null,
+    push: jest.fn(() => null),
+  })),
 }));
 
 jest.mock("@/app/_api_hooks/useSpørsmålOgSvar", () => ({
@@ -28,15 +29,71 @@ jest.mock("@/app/_api_hooks/useSpørsmålOgSvar", () => ({
   }),
 }));
 
+jest.mock("@/utils/CookieHandler", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      sesjonsID: "a",
+      sisteSvarteSporsmalId: jest.fn(),
+      clear: jest.fn(),
+      nyUndersokelse: jest.fn(),
+      oppdaterSisteSvarteSporsmal: jest.fn(),
+      finnesFraFør: false,
+    };
+  });
+});
+
+jest.mock("@/app/_api_hooks/svar", () => ({
+  postEnkeltSvar: jest.fn(() => Promise.resolve()),
+}));
+
 expect.extend(toHaveNoViolations);
 describe("Spørsmålsside", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   it("render fungerer", async () => {
     render(<Spørsmålsside params={{ uuid: "a", sporsmalId: "b" }} />);
 
     const tittel = await screen.findByText(dummySpørreundersøkelse[0].spørsmål);
 
     expect(tittel).toBeInTheDocument();
+
+    dummySpørreundersøkelse[0].svaralternativer.forEach(
+      (svar: svaralternativDTO) => {
+        expect(screen.getByText(svar.tekst)).toBeInTheDocument();
+      },
+    );
   });
+
+  it("klikk på svaralternativ", async () => {
+    render(<Spørsmålsside params={{ uuid: "a", sporsmalId: "b" }} />);
+
+    const svar = await screen.findByText(
+      dummySpørreundersøkelse[0].svaralternativer[0].tekst,
+    );
+    act(() => svar.click());
+
+    const neste = await screen.findByRole("button", { name: /Svar/i });
+    await act(async () => neste.click());
+
+    expect(postEnkeltSvar).toHaveBeenCalledTimes(1);
+  });
+
+  it("klikk på tilbake", async () => {
+    const pushFunction = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({
+      push: pushFunction,
+    });
+    render(<Spørsmålsside params={{ uuid: "a", sporsmalId: "b" }} />);
+    expect(pushFunction).toHaveBeenCalledTimes(0);
+
+    const tilbake = await screen.findByRole("button", { name: /Tilbake/i });
+    await act(async () => tilbake.click());
+
+    expect(pushFunction).toHaveBeenCalledTimes(1);
+    expect(pushFunction).toHaveBeenCalledWith("./b/tilbake");
+  });
+
   it("axe UU-test", async () => {
     const { container } = render(
       <Spørsmålsside params={{ uuid: "a", sporsmalId: "b" }} />,
