aefb4a00ad8246afbca594ea348e60a69a426597
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-02-29 11:56:05 +0100

    Fjernet unødvendige tester.

--- src/dom.test.tsx
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-
-test("dom test", () => {
-  render(<button>My button</button>);
-  const button = screen.getByRole("button");
-  expect(button).toBeInTheDocument();
-});
