0d5ab4bbc03686b0337fd81d1f64a4edd4118f88
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-03-12 11:35:18 +0100

    Fikset QR-kode som ikke ville rendre server side.

--- src/app/_components/QRkodeVisning.tsx
+++ src/app/_components/QRkodeVisning.tsx
@@ -1,3 +1,4 @@
+"use client";
 import startsideStyles from "@/app/[uuid]/vert/[vertId]/(start)/startside.module.css";
 import { QRCodeSVG } from "qrcode.react";
 import React from "react";
@@ -7,6 +8,19 @@ interface Props {
 }
 
 export function QRkodeVisning({ spørreundersøkelseId }: Props) {
+  // Hele mounted-staten er en work-around for at vi får lenke riktig i QR-koden, ved å ikke rendre server side.
+  const [mounted, setMounted] = React.useState<boolean>();
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  return <QRkodeVisningComponent spørreundersøkelseId={spørreundersøkelseId} />;
+}
+export function QRkodeVisningComponent({ spørreundersøkelseId }: Props) {
   return (
     <QRCodeSVG
       value={LenkeFraId(spørreundersøkelseId)}
@@ -16,5 +30,9 @@ export function QRkodeVisning({ spørreundersøkelseId }: Props) {
 }
 
 export function LenkeFraId(spørreundersøkelseId: string) {
+  if (typeof window === "undefined") {
+    return "";
+  }
+
   return `${window.location.protocol}//${window.location.host}/${spørreundersøkelseId}/deltaker`;
 }
