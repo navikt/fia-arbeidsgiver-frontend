85919208207d1ccfe700a114c45530dc09c9b561
Sindre Larsen <silar.dev@proton.me>
2024-03-26 12:58:20 +0100

    Styling på deltakerprogresjon

--- src/app/[uuid]/deltaker/[temaId]/[sporsmalId]/SpørsmålBleedDeltaker.tsx
+++ src/app/[uuid]/deltaker/[temaId]/[sporsmalId]/SpørsmålBleedDeltaker.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Bleed, BodyShort, Box, Heading, HStack } from "@navikt/ds-react";
+import {
+  Bleed,
+  BodyShort,
+  Box,
+  Heading,
+  HStack,
+  VStack,
+} from "@navikt/ds-react";
 import kartleggingStyles from "@/app/kartlegging.module.css";
 import deltakerStyles from "@/app/[uuid]/deltaker/[temaId]/[sporsmalId]/sporsmalsside.module.css";
 import React from "react";
@@ -16,10 +23,10 @@ export function SpørsmålBleedDeltaker({
       <Box padding="5" className={kartleggingStyles.bleedKlar}>
         <HStack className={deltakerStyles.bleedInnhold}>
           {spørsmålOgSvar && (
-            <>
+            <VStack>
               <Heading size="small">{spørsmålOgSvar.temabeskrivelse}</Heading>
-              <BodyShort>{`${spørsmålOgSvar.spørsmålnummer} av ${spørsmålOgSvar.antallSpørsmål}`}</BodyShort>
-            </>
+              <BodyShort>{`Spørsmål ${spørsmålOgSvar.spørsmålnummer} av ${spørsmålOgSvar.antallSpørsmål}`}</BodyShort>
+            </VStack>
           )}
         </HStack>
       </Box>
