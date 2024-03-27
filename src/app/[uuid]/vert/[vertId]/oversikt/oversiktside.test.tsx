9697cc580f11cde60922921faa6060a3c6a82361
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-03-27 10:17:34 +0100

    La inn test på navigasjonsknapper på oversiktside.

--- src/app/[uuid]/vert/[vertId]/oversikt/oversiktside.test.tsx
+++ src/app/[uuid]/vert/[vertId]/oversikt/oversiktside.test.tsx
@@ -1,32 +1,43 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import Oversiktside from "./page";
 import { axe, toHaveNoViolations } from "jest-axe";
 
 // @ts-ignore
 import { dummyTemaoversikt } from "@/utils/dummyData/vert";
+import { useTemaoversikt } from "@/app/_api_hooks/vert/useTemaoversikt";
+import { TemaStatus } from "@/app/_types/tema";
 
 expect.extend(toHaveNoViolations);
+
+const pushMock = jest.fn(() => null);
 
 jest.mock("next/navigation", () => ({
   useRouter: jest.fn(() => ({
     prefetch: () => null,
-    push: jest.fn(() => null),
+    push: pushMock,
   })),
   usePathname: jest.fn(() => "/"),
 }));
 
 jest.mock("@/app/_api_hooks/vert/useTemaoversikt", () => ({
-  useTemaoversikt: () => ({
+  useTemaoversikt: jest.fn(() => ({
     data: dummyTemaoversikt,
     isLoading: false,
     error: undefined,
-  }),
+  })),
 }));
 
 describe("Oversiktside", () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    jest.mocked(useTemaoversikt).mockReturnValue({
+      data: dummyTemaoversikt,
+      isLoading: false,
+      error: undefined,
+      mutate: jest.fn(),
+      isValidating: false,
+    });
   });
 
   test("rett innhold blir tegnet opp", async () => {
@@ -41,5 +52,81 @@ describe("Oversiktside", () => {
     );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
+  });
+
+  test("Andre tema er ikke åpnet før første tema er besvart", async () => {
+    jest.mocked(useTemaoversikt).mockReturnValue({
+      data: [
+        {
+          ...dummyTemaoversikt[0],
+          status: TemaStatus.ÅPNET,
+        },
+        {
+          ...dummyTemaoversikt[1],
+          status: TemaStatus.IKKE_ÅPNET,
+        },
+      ],
+      isLoading: false,
+      error: undefined,
+      mutate: jest.fn(),
+      isValidating: false,
+    });
+    render(<Oversiktside params={{ uuid: "uuid", vertId: "vertId" }} />);
+    const fortsettKnapp = screen.queryByText("Gjenoppta");
+    expect(fortsettKnapp).not.toBeInTheDocument();
+
+    const startknapper = screen.getAllByRole("button", { name: "Start" });
+    expect(startknapper).toHaveLength(2);
+    expect(startknapper[0]).not.toBeDisabled();
+    expect(startknapper[1]).toBeDisabled();
+
+    expect(pushMock).not.toHaveBeenCalled();
+
+    act(() => startknapper[0].click());
+    expect(pushMock).toHaveBeenCalledTimes(1);
+    expect(pushMock).toHaveBeenCalledWith(
+      `./tema/${dummyTemaoversikt[0].temaId}`,
+    );
+    act(() => startknapper[1].click());
+    expect(pushMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("Andre tema er åpnet når første tema er besvart", async () => {
+    jest.mocked(useTemaoversikt).mockReturnValue({
+      data: [
+        {
+          ...dummyTemaoversikt[0],
+          status: TemaStatus.ALLE_SPØRSMÅL_ÅPNET,
+        },
+        {
+          ...dummyTemaoversikt[1],
+          status: TemaStatus.ÅPNET,
+        },
+      ],
+      isLoading: false,
+      error: undefined,
+      mutate: jest.fn(),
+      isValidating: false,
+    });
+    render(<Oversiktside params={{ uuid: "uuid", vertId: "vertId" }} />);
+    const fortsettKnapp = screen.getByText("Gjenoppta");
+    expect(fortsettKnapp).toBeInTheDocument();
+    expect(fortsettKnapp).not.toBeDisabled();
+
+    const startknapper = screen.getAllByRole("button", { name: "Start" });
+    expect(startknapper).toHaveLength(1);
+    expect(startknapper[0]).not.toBeDisabled();
+
+    expect(pushMock).not.toHaveBeenCalled();
+    act(() => startknapper[0].click());
+    expect(pushMock).toHaveBeenCalledTimes(1);
+    expect(pushMock).toHaveBeenCalledWith(
+      `./tema/${dummyTemaoversikt[1].temaId}`,
+    );
+    act(() => fortsettKnapp.click());
+    expect(pushMock).toHaveBeenCalledTimes(2);
+    expect(pushMock).toHaveBeenCalledWith(
+      `./tema/${dummyTemaoversikt[0].temaId}`,
+    );
   });
 });
