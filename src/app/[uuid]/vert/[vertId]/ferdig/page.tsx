2497bae481c6328f78cfe258eac39cda480e5fa4
Sindre Larsen <silar.dev@proton.me>
2024-04-02 08:32:43 +0200

    endrer statiske ferdigside til react server component

--- src/app/[uuid]/vert/[vertId]/ferdig/page.tsx
+++ src/app/[uuid]/vert/[vertId]/ferdig/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import React from "react";
-import FerdigInnhold from "./FerdigInnhold";
+import { Bleed, BodyShort, Box, Heading, Page, VStack } from "@navikt/ds-react";
+import kartleggingStyles from "@/app/kartlegging.module.css";
+import { PageBlock } from "@navikt/ds-react/Page";
+import ferdigStyles from "./ferdigside.module.css";
 
 export const metadata: Metadata = {
   title: "Kartleggingsverktøy",
@@ -8,5 +11,25 @@ export const metadata: Metadata = {
 };
 
 export default function Ferdigside() {
-  return <FerdigInnhold />;
+  return (
+    <Page contentBlockPadding="none">
+      <PageBlock as={"main"}>
+        <Bleed marginInline="full" asChild>
+          <Box padding="5" className={kartleggingStyles.bleedFerdig}>
+            <Heading size={"large"} className={ferdigStyles.bleedInnhold}>
+              Ferdig!
+            </Heading>
+          </Box>
+        </Bleed>
+        <VStack gap="4">
+          <VStack align={"center"}>
+            <BodyShort size={"large"} spacing>
+              Bra jobbet! Da har alle svar blitt logget og sendt til Fia.
+              Rådgiveren sier litt om prosessen videre.
+            </BodyShort>
+          </VStack>
+        </VStack>
+      </PageBlock>
+    </Page>
+  );
 }
