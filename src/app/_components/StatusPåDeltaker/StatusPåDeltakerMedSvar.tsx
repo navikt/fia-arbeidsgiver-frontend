df915122e8c5fbf9b245e9d3d86b54e2d3570c53
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-04-12 08:40:53 +0200

    Endret til å rendre antall deltakere og antall svar i samme komponent på spørsmål vertssiden for å vise error penere.

--- src/app/_components/StatusPåDeltaker/StatusPåDeltakerMedSvar.tsx
+++ src/app/_components/StatusPåDeltaker/StatusPåDeltakerMedSvar.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { HStack } from "@navikt/ds-react";
+import { Alert, HStack, Loader } from "@navikt/ds-react";
 import spørsmålStyles from "@/app/[uuid]/vert/[vertId]/tema/[temaId]/[sporsmalId]/sporsmalsside.module.css";
 import { PersonGroupFillIcon } from "@navikt/aksel-icons";
 import React from "react";
-import { AntallSvar } from "@/app/_components/StatusPåDeltaker/AntallSvar";
-import { AntallDeltakere } from "@/app/_components/StatusPåDeltaker/AntallDeltakere";
+import { useAntallSvar } from "@/app/_api_hooks/vert/useAntallSvar";
+import kartleggingStyles from "@/app/kartlegging.module.css";
+import { useAntallDeltakere } from "@/app/_api_hooks/vert/useAntallDeltakere";
 
 export function StatusPåDeltakerMedSvar({
   spørreundersøkelseId,
@@ -18,20 +19,62 @@ export function StatusPåDeltakerMedSvar({
   temaId: number;
   spørsmålId: string;
 }) {
+  const {
+    data: antallSvar,
+    isLoading: lasterAntallSvar,
+    error: feilAntallSvar,
+  } = useAntallSvar({
+    vertId,
+    spørreundersøkelseId,
+    temaId,
+    spørsmålId,
+  });
+  const {
+    data: antallDeltakere,
+    isLoading: lasterAntallDeltakere,
+    error: feilAntallDeltakere,
+  } = useAntallDeltakere({
+    vertId,
+    spørreundersøkelseId,
+  });
+
+  if (feilAntallSvar !== undefined || feilAntallDeltakere !== undefined) {
+    return (
+      <Alert
+        variant={"warning"}
+        inline
+        className={kartleggingStyles.alertWarning}
+      >
+        {feilAntallSvar?.message}
+        {feilAntallSvar !== undefined && feilAntallDeltakere !== undefined ? (
+          <br />
+        ) : undefined}
+        {feilAntallDeltakere?.message}
+      </Alert>
+    );
+  }
   return (
     <HStack className={spørsmålStyles.deltakere}>
       <PersonGroupFillIcon />
-      <AntallSvar
-        spørreundersøkelseId={spørreundersøkelseId}
-        vertId={vertId}
-        temaId={temaId}
-        spørsmålId={spørsmålId}
-      />{" "}
-      av{" "}
-      <AntallDeltakere
-        spørreundersøkelseId={spørreundersøkelseId}
-        vertId={vertId}
+      <InnholdEllerSpinner innhold={antallSvar} laster={lasterAntallSvar} />
+      {" av "}
+      <InnholdEllerSpinner
+        innhold={antallDeltakere}
+        laster={lasterAntallDeltakere}
       />
     </HStack>
   );
+}
+
+function InnholdEllerSpinner({
+  innhold,
+  laster,
+}: {
+  innhold: React.ReactNode;
+  laster: boolean;
+}) {
+  if (innhold === undefined || laster) {
+    return <Loader variant="inverted" />;
+  }
+  return <>{innhold}</>;
 }
