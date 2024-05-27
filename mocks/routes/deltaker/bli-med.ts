51807e74f5c21ada0ec2a2f20f588ec37aa5fcbe
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-05-27 12:49:21 +0200

    Flyttet dummydata som bare blir brukt i en fil til filen den brukes i.

--- mocks/routes/deltaker/bli-med.ts
+++ mocks/routes/deltaker/bli-med.ts
@@ -1,7 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { dummyBliMed } = require("@/utils/dummydata/deltaker");
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { API_DELTAKER_BLI_MED_URL } = require("@/utils/urls");
+
+const dummyBliMed = {
+  spørreundersøkelseId: "e2f863df-309e-4314-9c7e-c584237fd90a",
+  sesjonsId: "790fba4d-1c9f-4c20-a019-d574d9542421",
+};
 
 const bliMedRoutes = [
   {
