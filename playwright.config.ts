9ff47e6dbb1f9b30a9e7ba6925f333b324424543
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-05-27 13:56:04 +0200

    Prøver med production build for å kjøre playwright-tester.

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: [
     {
-      command: "bun dev",
+      command: "bun run build && bun run start",
       url: "http://localhost:3000",
       reuseExistingServer: true,
       stderr: "pipe",
