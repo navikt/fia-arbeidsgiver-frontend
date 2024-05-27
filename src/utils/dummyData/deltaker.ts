cacec7d65c0c7904758acefef4814fd209e397ac
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-05-27 12:49:21 +0200

    Flyttet dummydata som bare blir brukt i en fil til filen den brukes i.

--- src/utils/dummyData/deltaker.ts
+++
@@ -1,8 +0,0 @@
-const dummyBliMed = {
-  spørreundersøkelseId: "e2f863df-309e-4314-9c7e-c584237fd90a",
-  sesjonsId: "790fba4d-1c9f-4c20-a019-d574d9542421",
-};
-
-module.exports = {
-  dummyBliMed,
-};
