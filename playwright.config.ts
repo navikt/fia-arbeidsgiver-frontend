3ceb0e45ceb8bc99b74d7a7afeefdd49d48f5c9e
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-06-05 15:12:49 +0200

    Fjernet unødvendige kommentarer fra configfil.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig, devices } from "@playwright/test";
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 const isCI = !!process.env.CI;
@@ -31,10 +25,6 @@ export default defineConfig({
     isCI ? ["github"] : ["line"],
   ],
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     screenshot: "on",
     video: "retry-with-video",
@@ -105,9 +95,4 @@ export default defineConfig({
       stdout: "ignore",
     },
   ],
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
