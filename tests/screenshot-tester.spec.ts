baad886df18aab2919ea178c438b68db0131544d
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2024-06-10 15:01:32 +0200

    Fjerner tester som var lagt til for å debugge playwright i github actions.

--- tests/screenshot-tester.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test.describe("screenshots", () => {
-  test("Screenshot av innhold likner", async ({ page }) => {
-    await page.goto("http://localhost:3000/screenshot-test");
-
-    await expect(page).toHaveScreenshot({ fullPage: true });
-  });
-});
