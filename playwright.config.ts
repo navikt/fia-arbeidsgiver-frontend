d30b82093931a8332d8fb6f89e0d61ad2203d92e
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-08-07 13:25:17 +0200

    Tester om vi kan løse screenshotforskjeller mellom mac og CI med browserflagg.

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,12 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          args: ["--disable-font-subpixel-positioning", "--disable-lcd-text"],
+        },
+      },
     },
 
     {
